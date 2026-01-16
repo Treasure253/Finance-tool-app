9188a305bb2c6791677fc80ca4c759295aa1a109
FAITH ORILABAWAYE <faithorilabawaye@gmail.com>
2026-01-16 05:50:42 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Finance-tool-app/",
+  base: "/", // ✅ FIXED FOR VERCEL
   plugins: [react()],
 })
